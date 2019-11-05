191f6dfd16fe27334d94a55207679cef4e5cdeec
Keisha <keisha.q06@gmail.com>
2019-11-05 00:27:54 -0800

    merged from keisha_redesign_splash with basic working drawer

--- app/src/main/java/com/example/virtualcloset/MainActivity.java
+++ app/src/main/java/com/example/virtualcloset/MainActivity.java
@@ -1,36 +1,54 @@
 package com.example.virtualcloset;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
+import androidx.appcompat.widget.Toolbar;
 
 import in.goodiebag.carouselpicker.CarouselPicker;
 import java.util.List;
 import java.util.ArrayList;
+
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.viewpager.widget.ViewPager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.navigation.NavigationView;
+import android.view.MenuItem;
+import androidx.core.view.GravityCompat;
+
+
+
 public class MainActivity extends AppCompatActivity {
+    private DrawerLayout mDrawer;
+    private Toolbar toolbar;
+    private NavigationView nvDrawer;
+
+    private ActionBarDrawerToggle drawerToggle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ImageView cameraActvivityButton = this.findViewById(R.id.startCameraActivityButton);
-        cameraActvivityButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, CameraActivity.class);
-                startActivity(intent);
-            }
-        });
+        //setup toolbar to replace actionbar
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        //change later to hamburger (three lines icon)
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mDrawer = findViewById(R.id.drawer_layout);
+        nvDrawer = findViewById(R.id.nvView);
+        setupDrawerContent(nvDrawer);
+
+
         final CarouselPicker carouselPicker = (CarouselPicker) findViewById(R.id.carousel);
         CarouselPicker carouselPicker2 =  (CarouselPicker) findViewById(R.id.carousel2);
 
-        // Case 1 : To populate the picker with images
+// Case 1 : To populate the picker with images
         final List<CarouselPicker.PickerItem> shirts = new ArrayList<>();
         shirts.add(new CarouselPicker.DrawableItem(R.drawable.striped_guess));
         shirts.add(new CarouselPicker.DrawableItem(R.drawable.carhart_shirt));
@@ -40,12 +58,10 @@ public class MainActivity extends AppCompatActivity {
         bottoms.add(new CarouselPicker.DrawableItem(R.drawable.jeans));
         bottoms.add(new CarouselPicker.DrawableItem(R.drawable.black_jeans));
         bottoms.add(new CarouselPicker.DrawableItem(R.drawable.striped_pants));
-        
-        //Create an adapter
+//Create an adapter
         CarouselPicker.CarouselViewAdapter imageAdapter = new CarouselPicker.CarouselViewAdapter(this, shirts, 0);
         CarouselPicker.CarouselViewAdapter imageAdapter2 = new CarouselPicker.CarouselViewAdapter(this, bottoms, 0);
-        
-        //Set the adapter
+//Set the adapter
         carouselPicker.setAdapter(imageAdapter);
         carouselPicker2.setAdapter(imageAdapter2);
 
@@ -69,4 +85,58 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // The action bar home/up action should open or close the drawer.
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawer.openDrawer(GravityCompat.START);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void setupDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        selectDrawerItem(menuItem);
+                        return true;
+                    }
+                });
+    }
+
+    public void selectDrawerItem(MenuItem menuItem) {
+        // Create a new fragment and specify the fragment to show based on nav item clicked
+        Fragment fragment = null;
+        Class fragmentClass;
+        switch(menuItem.getItemId()) {
+            case R.id.nav_first_fragment:
+                fragmentClass = TestFragment1.class;
+                break;
+            default:
+                fragmentClass = TestFragment1.class;
+        }
+
+        try {
+            fragment = (Fragment) fragmentClass.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Insert the fragment by replacing any existing fragment
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.flContent, fragment).commit();
+
+        // Highlight the selected item has been done by NavigationView
+        menuItem.setChecked(true);
+        // Set action bar title
+        setTitle(menuItem.getTitle());
+        // Close the navigation drawer
+        mDrawer.closeDrawers();
+    }
 }
+
