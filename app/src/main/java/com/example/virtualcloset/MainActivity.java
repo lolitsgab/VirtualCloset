f760bcbcdc57efda2b124fb12a1898e89517a337
Nicole Ng <nmng@ucsc.edu>
2019-11-07 16:53:34 -0800

    retrieves all top and bottom clothing pieces from firebaase storage and displays in carousel

--- app/src/main/java/com/example/virtualcloset/MainActivity.java
+++ app/src/main/java/com/example/virtualcloset/MainActivity.java
@@ -1,11 +1,7 @@
 package com.example.virtualcloset;
-
-import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
@@ -13,29 +9,27 @@ import java.util.ArrayList;
 import androidx.annotation.NonNull;
 
 import java.util.List;
-import java.util.UUID;
 import in.goodiebag.carouselpicker.CarouselPicker;
 import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.ListResult;
 import com.google.firebase.storage.StorageReference;
-import com.synnapps.carouselview.CarouselView;
-import com.synnapps.carouselview.ImageListener;
+
 
 
 public class MainActivity extends AppCompatActivity {
-    String  UserUID, uniqueImageName, top;
+    String  UserUID;
     Bitmap bm;
-    ImageView imageView;
-
-    StorageReference storageReference, pathTopReference;
+    List<CarouselPicker.PickerItem> topItems = new ArrayList<>();
+    List<CarouselPicker.PickerItem> bottomItems = new ArrayList<>();
+    StorageReference storageReference, pathTopReference, pathBottomReference;
     ImageView cameraActvivityButton;
     FirebaseStorage storage;
     CarouselPicker topCarousel, bottomCarousel;
-    Context ctx;
-
+    final long ONE_MEGABYTE = 1024 * 1024;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,40 +37,82 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         cameraActvivityButton = this.findViewById(R.id.startCameraActivityButton);
         topCarousel = this.findViewById(R.id.carouselTop);
+        bottomCarousel = this.findViewById(R.id.carouselBottom);
 
+        // FIREBASE SETUP
         storage = FirebaseStorage.getInstance();
         storageReference = storage.getReference();
         UserUID = FirebaseAuth.getInstance().getUid();
-        pathTopReference = storageReference.child("users/" + "hsC42FZunCgmKEhZNeRKEATks6e2" +
-                "/clothes/" + "top" + "/" + "021e9412-c89c-4e08-967e-78216ff8f8d5");
 
-        //System.out.println("path:" + pathTopReference);
-        // listener needs to be inside view, but values cannot be
-        // iterated inside the view
+        // TOP CAROUSEL
+        pathTopReference = storageReference.child("users/" + UserUID +
+                "/clothes/" + "top" + "/");
+        pathTopReference.listAll().addOnSuccessListener(new OnSuccessListener<ListResult>() {
+            @Override
+            public void onSuccess(ListResult listResult) {
 
+                for(StorageReference filteref: listResult.getItems()) {
+                    filteref.getBytes(ONE_MEGABYTE).addOnSuccessListener(new OnSuccessListener<byte[]>() {
+                        @Override
+                        public void onSuccess(byte[] bytes) {
+                            bm = BitmapFactory.decodeByteArray(bytes, 0 , bytes.length);
+                            topItems.add(new CarouselPicker.BitmapItem(bm));
+                            CarouselPicker.CarouselViewAdapter topAdapter = new CarouselPicker.CarouselViewAdapter
+                                    (getApplicationContext(), topItems, 0);
+                            topCarousel.setAdapter(topAdapter);
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+
+                        }
+                    });
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+
+            }
+        });
+
+        // BOTTOM CAROUSEL
+        pathBottomReference = storageReference.child("users/" + UserUID +
+                "/clothes/" + "bottom" + "/");
+        pathBottomReference.listAll().addOnSuccessListener(new OnSuccessListener<ListResult>() {
+            @Override
+            public void onSuccess(ListResult listResult) {
+                for(StorageReference filteref: listResult.getItems()) {
+                    filteref.getBytes(ONE_MEGABYTE).addOnSuccessListener(new OnSuccessListener<byte[]>() {
+                        @Override
+                        public void onSuccess(byte[] bytes) {
+                            bm = BitmapFactory.decodeByteArray(bytes, 0 , bytes.length);
+                            bottomItems.add(new CarouselPicker.BitmapItem(bm));
+                            CarouselPicker.CarouselViewAdapter bottomAdapter = new CarouselPicker.CarouselViewAdapter
+                                    (getApplicationContext(), bottomItems, 0);
+                            bottomCarousel.setAdapter(bottomAdapter);
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+
+                        }
+                    });
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+
+            }
+        });
+
+        // CAMERA ACTIVITY
         cameraActvivityButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, CameraActivity.class);
                 startActivity(intent);
-            }
-        });
-        final long ONE_MEGABYTE = 1024 * 1024;
-        pathTopReference.getBytes(ONE_MEGABYTE).addOnSuccessListener(new OnSuccessListener<byte[]>() {
-            @Override
-            public void onSuccess(byte[] bytes) {
-                bm = BitmapFactory.decodeByteArray(bytes, 0 , bytes.length);
-                List<CarouselPicker.PickerItem> bitItems = new ArrayList<>();
-                System.out.println(bm);
-                bitItems.add(new CarouselPicker.BitmapItem(bm));
-                bitItems.add(new CarouselPicker.BitmapItem(bm));
-                bitItems.add(new CarouselPicker.BitmapItem(bm));
-                CarouselPicker.CarouselViewAdapter bitAdapter = new CarouselPicker.CarouselViewAdapter(getApplicationContext(), bitItems, 0);
-                topCarousel.setAdapter(bitAdapter);
-            }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception exception) {
             }
         });
     }
